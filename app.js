a62f8610ea9250d3f009110ff516918d0e530c6a
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2018-03-01 10:03:35 +0200

    Part of the UI and data fetch in place

---
+++ app.js
@@ -0,0 +1,14 @@
+// Init weather object
+const weather = new Weather('Boston', 'MA');
+// Init UI
+const ui = new UI;
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather)
+
+// weather.changeLocation('Bucharest', 'RO')
+
+function getWeather() {
+  weather.getWeather()
+    .then(results => ui.paint(results))
+    .catch(err => console.log(err));
+}
